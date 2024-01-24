a6ad468e22596abdd18a55cfdb3320f3327bc1ab
masow553 <156091177+masow553@users.noreply.github.com>
2024-01-24 12:40:02 +0100

    Update my_script.py

--- my_script.py
+++ my_script.py
@@ -7,8 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1ppFrKCDrRKcDvJzP8ZkzHo9WVIwXLW7F
 """
 
-pip install streamlit
-
 import streamlit as st
 import pandas as pd
 
